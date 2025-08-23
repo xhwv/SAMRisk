ffdc709d9d8df129d404d0d07f613b4fcab45517
xhwv <37981661+xhwv@users.noreply.github.com>
2025-08-23 12:34:28 +0800

    Update build_sam.py

--- segment_anything/build_sam.py
+++ segment_anything/build_sam.py
@@ -1,9 +1,7 @@
 import torch
 
 from functools import partial
-from .modeling.earlyfusion import earlyfusion
 from .modeling.image_encoder3D import ImageEncoderViT3D
-from .modeling.image_encoder3D_Lora import ImageEncoderViT3Dlora
 from .modeling.mask_decoder3D import MaskDecoder3D
 from .modeling.prompt_encoder3D import PromptEncoder3D
 from .modeling.sam3D import Sam3D
@@ -122,22 +120,6 @@ def _build_sam3D_ori(
     vit_patch_size = 16
     image_embedding_size = image_size // vit_patch_size
     sam = Sam3D(
-        image_emb=earlyfusion(),
-        # image_emb=earlyfusion(dim=4),
-        image_encoder_lora=ImageEncoderViT3Dlora(
-            depth=encoder_depth,
-            embed_dim=encoder_embed_dim,
-            img_size=image_size,
-            mlp_ratio=4,
-            norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
-            num_heads=encoder_num_heads,
-            patch_size=vit_patch_size,
-            qkv_bias=True,
-            use_rel_pos=True,
-            global_attn_indexes=encoder_global_attn_indexes,
-            window_size=14,
-            out_chans=prompt_embed_dim,
-        ),
         image_encoder=ImageEncoderViT3D(
             depth=encoder_depth,
             embed_dim=encoder_embed_dim,
